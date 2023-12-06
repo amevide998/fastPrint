d3a27e4fed2465aecca72815ae77a8f1bc4809e1
amevide998 <hdsvidi@gmail.com>
2023-12-06 07:45:19 +0700

    increase timeout

--- fastprint/settings.py
+++ fastprint/settings.py
@@ -83,6 +83,9 @@ DATABASES = {
         'PASSWORD': 'HugMUI8iSzbnBKYD',
         'HOST': 'db.lpdqivjetkhlvzeurknk.supabase.co',
         'PORT': '5432',
+        'OPTIONS': {
+            'connect_timeout': 30
+        }
     }
 }
 
